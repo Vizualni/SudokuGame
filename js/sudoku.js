6115ed894b6f3f7a99d2592864960eb37b795111
Matija Martinic <crazy4magic@gmail.com>
2016-01-05 17:12:37 +0100

    please work

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,10 +1,14 @@
-var Sudoku = function () {
+var Sudoku = function (firstBoard) {
 	this._SIZE = 9;
 	this.emptyField = 0;
-	this.init();
+	if (firstBoard===undefined) {
+		this.createEmptyBoard();
+	} else {
+		this.prefill(firstBoard);
+	}
 }
 
-Sudoku.prototype.init = function() {
+Sudoku.prototype.createEmptyBoard = function() {
 	this.board = new Array(this._SIZE);
 	for(var i=0; i<this._SIZE; i++) {
 		this.board[i] = new Array(this._SIZE).fill(0);
@@ -12,11 +16,55 @@ Sudoku.prototype.init = function() {
 	console.log(this.board);
 };
 
+Sudoku.prototype.prefill = function(firstBoard) {
+	// firstBoard is string with . for empty field
+	if (this.board!==undefined) {
+		throw "It's already created";
+	}
+	if (firstBoard.length!=81) {
+		throw "Board is the wrong size";
+	}
+	var board = new Array(this._SIZE);
+	for(var i=0; i<this._SIZE; i++) {
+		board[i] = new Array(this._SIZE).map(function(d){if(d=='.')return 0; return parseInt(d)});
+	}
+	if (this.isValid()==false) {
+		throw "String is not valid";
+	}
+	this.board = board;
+}
+
 Sudoku.prototype.nextMove = function(x, y, digit) {
 	// returns false if current x,y,digit is not valid move
 	// else it checks if its solved and returs true
 	// if it's solved, then event called win is triggered
+
+	this.set(x, y, digit);
+	
+	var everythingFilled = this.isEverythingFilled();
+	var validBoard = this.isValid();
+
+	if (everythingFilled && validBoard) {
+		// solved
+		this.win();
+		return true;
+	}
+
+	if (!validBoard) {
+		return false;
+	}
+	
+	return undefined;
+
 };
+
+Sudoku.prototype.set = function(x, y, digit) {
+	if (isNaN(digit) || digit=='0') {
+		throw "Third argument must be digit: 1, 2, 3, 4, 5, 6, 7, 8, 9.";
+	}
+	digit = parseInt(digit);
+	this.board[x][y] = digit;
+}
 
 Sudoku.prototype.get = function(x, y) {
 	return this.board[x][y];
@@ -31,16 +79,6 @@ Sudoku.prototype.isEverythingFilled = function() {
 		}
 	}
 	return true;
-};
-
-Sudoku.prototype.isSolved = function() {
-	// if it has empty spaces then it's not solved
-	if (this.isEverythingFilled()==false) {
-		return false;
-	}
-
-	// check all rows 
-
 };
 
 Sudoku.prototype.isArrayOfNumbersValid = function(arrayOfNumber) {
@@ -59,8 +97,28 @@ Sudoku.prototype.isArrayOfNumbersValid = function(arrayOfNumber) {
 }
 
 Sudoku.prototype.isValid = function() {
-	
+	// checking for each row
+	var arrayOfNumber = [];
+	for(var x in this.board) {
+		arrayOfNumber = [];
+		for(var y in this.board[x]) {
+			arrayOfNumber.push(this.board[x][y]);
+		}
+		if (this.isArrayOfNumbersValid(arrayOfNumber)==false) {
+			return false;
+		}
+	}
 
+	for(var x in this.board) {
+		arrayOfNumber = [];
+		for(var y in this.board[x]) {
+			arrayOfNumber.push(this.board[y][x]);
+		}
+		if (this.isArrayOfNumbersValid(arrayOfNumber)==false) {
+			return false;
+		}
+	}
+	return true;
 };
 
 
